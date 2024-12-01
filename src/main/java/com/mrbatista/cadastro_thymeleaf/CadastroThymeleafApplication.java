50322179e2266111aa0952031821e3a45558feb7
geibatista <geibatista442@gmail.com>
2024-12-01 20:20:53 -0300

    Projeto iniciado

---
+++ src/main/java/com/mrbatista/cadastro_thymeleaf/CadastroThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.mrbatista.cadastro_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrbatista/cadastro_thymeleaf/CadastroThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbatista.cadastro_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
